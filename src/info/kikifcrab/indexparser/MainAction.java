e1810aa05f5548f026f363bf480e137eebf594fb
fcrab <evarasseed@gmail.com>
2012-12-04 17:44:10 +0800

    testFile

--- src/info/kikifcrab/indexparser/MainAction.java
+++ src/info/kikifcrab/indexparser/MainAction.java
@@ -1,14 +1,24 @@
 package info.kikifcrab.indexparser;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+import org.htmlparser.Attribute;
 import org.htmlparser.Node;
 import org.htmlparser.Parser;
+import org.htmlparser.nodes.AbstractNode;
+import org.htmlparser.nodes.TagNode;
+import org.htmlparser.tags.InputTag;
 import org.htmlparser.util.NodeIterator;
+import org.htmlparser.util.NodeList;
 import org.htmlparser.util.ParserException;
 import org.htmlparser.visitors.TextExtractingVisitor;
 
@@ -30,22 +40,75 @@ public class MainAction {
 		try{
 			ParseTools tool=new ParseTools();
 			//读取并解析整个HTML
-			Parser parser=new Parser((new URL(TARGET_ADDR)).openConnection());			
+			//通过URL获取在线HTML（用来做爬虫就行）
+//			Parser parser=new Parser((new URL(TARGET_ADDR)).openConnection());			
+			//通过读取自身文件获取
+			Parser parser=new Parser(readHTML("E:\\parserTest\\index.htm"));
+			
+			
 			TextExtractingVisitor visitor=new TextExtractingVisitor();
 //			parser.visitAllNodesWith(visitor);
 //			String textNodePage=visitor.getExtractedText();
 			
 			//打印该HTML节点内容
 //			tool.printMsg(textNodePage);
-			
+/*
 			for(NodeIterator i=parser.elements();i.hasMoreNodes();){
 				Node node=i.nextNode();
 				tool.getNodes(node);
 			}
+	*/		
+			//打印过滤的节点内容
+			NodeList filterNodeList=tool.getFilterTag(parser);
+			String testATTR="";
+			if(filterNodeList!=null){
+				for(int i=0;i<filterNodeList.size();i++){
+					TagNode tmlNode=(TagNode)filterNodeList.elementAt(i);
+					tool.getNodes(tmlNode);			//打印信息					
+			
+					String tmlPic=tmlNode.getAttribute("src");
+//					System.out.println(tmlPic);
+					//对符合条件的节点进行处理
+					tmlNode.setAttribute("src", "it's been set");
+					testATTR=tmlNode.toHtml();
+					if(tmlNode.toHtml().equals(testATTR))
+						System.out.println("true");
+//					parser.reset();
+//					System.out.print(testATTR);			//打印信息
+				}
+			}
+ 
+			//将新内容写入新文件
+			String saveFileName="E:\\parserTest\\index_new.htm";
+			String newContent="";
+			writeHTML(saveFileName,newContent);
 			
 		}catch(Exception e){
 			
 		}
+	}
+	//读取文件
+	private static String readHTML(String fileName){			//名称中要包含地址
+		System.out.println(fileName);
+		String fileData = new String();
+		try{
+			File file=new File(fileName);
+			BufferedReader bis=new BufferedReader(new FileReader(file));
+			String tmlLine=null;
+			while((tmlLine=bis.readLine())!=null){
+				fileData+=tmlLine;
+			}
+			bis.close();
+		}catch(Exception e){
+			System.out.print("read error");
+		}
+		return fileData;
+	}
+	
+	//写入文件
+	private static void writeHTML(String fileName,String content){
+		File newIndex=new File(fileName);
+		
 	}
 
 }
--- src/info/kikifcrab/indexparser/ParseTools.java
+++ src/info/kikifcrab/indexparser/ParseTools.java
@@ -7,6 +7,9 @@ import java.io.UnsupportedEncodingException;
 import org.htmlparser.Node;
 import org.htmlparser.NodeFilter;
 import org.htmlparser.Parser;
+import org.htmlparser.filters.AndFilter;
+import org.htmlparser.filters.HasAttributeFilter;
+import org.htmlparser.filters.HasChildFilter;
 import org.htmlparser.filters.TagNameFilter;
 import org.htmlparser.util.NodeList;
 import org.htmlparser.util.ParserException;
@@ -15,25 +18,32 @@ public class ParseTools {
 
 	//处理每个点
 	public void getNodes(Node node){
-		printMsg("getText:"+node.getText());				//文本信息
-		printMsg("getPlainText:"+node.toPlainTextString());	//纯文本信息
+//		printMsg("getText:"+node.getText());				//文本信息
+//		printMsg("getPlainText:"+node.toPlainTextString());	//纯文本信息
 		printMsg("toHTML:"+node.toHtml());					//原始HTML	
-		printMsg("toHTML(true):"+node.toHtml(true));
-		printMsg("toHTML(false):"+node.toHtml(false));
-		printMsg("toString:"+node.toString());				//字符串信息
+//		printMsg("toHTML(true):"+node.toHtml(true));
+//		printMsg("toHTML(false):"+node.toHtml(false));
+//		printMsg("toString:"+node.toString());				//字符串信息
 //		printMsg("toPage"+node.getPage());					//取得 node对应的page对象
-		printMsg("getStartPosition:"+node.getStartPosition());	//起始位置
-		printMsg("getEndPosition:"+node.getEndPosition());		//结束位置
+//		printMsg("getStartPosition:"+node.getStartPosition());	//起始位置
+//		printMsg("getEndPosition:"+node.getEndPosition());		//结束位置
 		printMsg("==============================我是分割线=======================");
 	}
 	//过滤器
 	public NodeList getFilterTag(Parser parser){
 		try {
 			NodeFilter filter=new TagNameFilter("img");		//根据标签名称
+			NodeFilter filter1=new HasAttributeFilter("id");	//包含id属性
+			//拥有某种子节点的节点
+			NodeFilter innerFilter=new TagNameFilter("DIV");
+			NodeFilter filter2=new HasChildFilter(innerFilter);
+			//逻辑运算filter
+			//and运算
+			NodeFilter logoFilter=new AndFilter(filter,filter1);
 			
 			//执行过滤器
-
-			NodeList nodes=parser.extractAllNodesThatMatch(filter);
+			
+			NodeList nodes=parser.extractAllNodesThatMatch(logoFilter);
 			return nodes;
 		} catch (ParserException e) {
 			// TODO Auto-generated catch block
@@ -41,6 +51,33 @@ public class ParseTools {
 		}
 		return null;
 	}
+	//条件过滤器（带过滤条件）		//type:过滤类型	//带标签
+	public NodeList OptionalFilter(Parser parser,int type,String optional){
+		NodeList nodes=null;
+		NodeFilter filter=null;
+		try{
+			switch(type){
+				case 0:					//获取LOGO标签
+					NodeFilter filter1=new TagNameFilter("img");		//根据标签名称
+					NodeFilter filter2=new HasAttributeFilter("id");	//包含id属性
+					filter=new AndFilter(filter1,filter2);
+					break;
+				case 1:					//
+					break;
+				case 2:
+					break;
+				default:break;
+			}
+			nodes=parser.extractAllNodesThatMatch(filter);
+			
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		
+		
+		return nodes;
+	}
+	
 	
 	//规定编码打印信息(默认GBK)
 	public void printMsg(String msg){
